1a488da3970f85bb641f31389e6c50479828fee0
qiuz <909168154@qq.com>
2019-07-22 18:03:15 +0800

    初始提交

---
+++ power/src/main/java/com/anying/AppPowerServer.java
@@ -0,0 +1,16 @@
+package com.anying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @author: qiuz
+ * @date: 2019/7/22 17:58
+ */
+@SpringBootApplication
+public class AppPowerServer {
+    public static void main(String[] args) {
+        SpringApplication.run(AppPowerServer.class);
+    }
+}
---
+++ user/src/main/java/com/anying/AppUserClient.java
@@ -0,0 +1,16 @@
+package com.anying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @author: qiuz
+ * @date: 2019/7/22 17:52
+ */
+@SpringBootApplication
+public class AppUserClient {
+    public static void main(String[] args) {
+        SpringApplication.run(AppUserClient.class);
+    }
+}
